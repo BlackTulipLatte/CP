efe53a7d580f8d01de04da4c8d316aca2a3ea519
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2022-06-06 22:23:54 -0400

    Leetcode test

---
+++ Java/lc14.java
@@ -0,0 +1,22 @@
+package Java;
+import java.util.*;
+
+public class lc14{
+
+    public static void main (String []args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("hello");
+
+    }
+
+    public static String longestCommonPrefix(String arr[]){
+
+        if(true){
+
+        }
+        return "";
+
+    }
+
+}
